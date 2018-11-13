e28be9bc40cf34c3d877c0675d76012ab7da4615
jalvarezotero <you@example.com>
2018-11-13 10:28:41 +0100

    comentario Clase_B

---
+++ RamasColaboradores/src/ramascolaboradores/RamasColaboradores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramascolaboradores;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+public class RamasColaboradores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //codigo de la clase_B 
+    }
+    
+}
